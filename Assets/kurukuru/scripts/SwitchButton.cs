3f1fd01dc0089f6de8889d75e6ab77b77c1b1881
mkd214 <mkd214@gmail.com>
2022-01-29 16:36:26 +0900

    Player周りの不具合修正

--- Assets/kurukuru/scripts/SwitchButton.cs
+++ Assets/kurukuru/scripts/SwitchButton.cs
@@ -30,13 +30,13 @@ public class SwitchButton : MonoBehaviour
 	// 箱の下の方にある当たり判定です。
 	private BoxCollider2D _hitCheckBoxCollider2D;
 
-	private BoxCollider2D _playerBoxCollider2D;
+	private CapsuleCollider2D _playerCapsuleCollider2D;
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		_hitCheckBoxCollider2D = hitCheck.GetComponent<BoxCollider2D>();
-		_playerBoxCollider2D = GameObject.Find("Player").GetComponent<BoxCollider2D>();
+		_playerCapsuleCollider2D = GameObject.Find("Player").GetComponent<CapsuleCollider2D>();
 	}
 
 	// Update is called once per frame
@@ -47,7 +47,7 @@ public class SwitchButton : MonoBehaviour
 
 	private void OnCollisionEnter2D(Collision2D other)
 	{
-		if (_hitCheckBoxCollider2D.IsTouching(_playerBoxCollider2D)
+		if (_hitCheckBoxCollider2D.IsTouching(_playerCapsuleCollider2D)
 		&& other.gameObject.CompareTag("Player"))
 		{
 			var manager = FindObjectOfType<GimmickManager>();
