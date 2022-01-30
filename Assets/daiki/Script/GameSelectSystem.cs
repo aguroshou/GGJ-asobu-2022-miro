a42b863088810907099f22e2b64ab221f85e5b0c
azuma-daiki <daiki0412.training@gmail.com>
2022-01-30 15:20:40 +0900

    リザルト画面とタイトル画面を変更しました。

--- Assets/daiki/Script/GameSelectSystem.cs
+++ Assets/daiki/Script/GameSelectSystem.cs
@@ -6,47 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class GameSelectSystem : MonoBehaviour
 {
-    public int Stage_num;
-    public GameObject Level1;
-    public GameObject Level2;
-    public GameObject Level3;
-    public GameObject CLEAR1;
-    public GameObject CLEAR2;
-    public GameObject CLEAR3;
-
-    void Start()
-    {
-        //現在のStage_numを呼び出す
-        Stage_num = PlayerPrefs.GetInt("STAGE",0);
-    }
-
-    void Update()
-    {
-        if(Stage_num == 1)
-        {
-            //Stage_numが2以上のとき、ステージ1の画像をクリア画像に変更
-            Level1.SetActive(false);
-            CLEAR1.SetActive(true);
-        }
-
-        if (Stage_num == 2)
-        {
-            //Stage_numが3以上のとき、ステージ1の画像をクリア画像に変更
-            Level2.SetActive(false);
-            CLEAR2.SetActive(true);
-        }
-
-        if (Stage_num == 3)
-        {
-            //Stage_numが3以上のとき、ステージ1の画像をクリア画像に変更
-            Level3.SetActive(false);
-            CLEAR3.SetActive(true);
-        }
-    }
 
     public void OnClickLevel1Button()
     {
-        SceneManager.LoadScene("Stage1");
+        SceneManager.LoadScene("ClearScene");
     }
 
     public void OnClickLevel2Button()
---
+++ Assets/daiki/Script/StageClear1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StageClear1 : MonoBehaviour
+{
+    public void OnClickStageSelectButton()
+    {
+        SceneManager.LoadScene("GameSelect");
+    }
+
+    public void OnClickNextStageButton()
+    {
+        SceneManager.LoadScene("Stage2");
+    }
+}
